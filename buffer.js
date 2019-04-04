3a5bcd93f59871c70a28c9e2cd089bf6b4ca3f4e
Cara Ponzini <ponzini@gmail.com>
2019-04-04 16:29:45 -0700

    buffer fun plus file setup

---
+++ buffer.js
@@ -0,0 +1,21 @@
+const buffer = Buffer.alloc(5);
+const stringBuffer = Buffer.from('I am the flying spaghetti monster');
+const hello = Buffer.from([0x48, 0x45, 0x4C, 0x4C, 0x4f, 0x3F]);
+const newBuffer = Buffer.alloc(6);
+newBuffer.writeInt8(0x48, 0);
+newBuffer.writeInt8(0x45, 1);
+newBuffer.writeInt8(0x4C, 2);
+newBuffer.writeInt8(0x4C, 3);
+newBuffer.writeInt8(0x4F, 4);
+newBuffer.writeInt8(0x21, 5);
+console.log(newBuffer.toString());
+
+console.log(stringBuffer.toString());
+
+const mapped = buffer.map(item => item = 63)
+console.log(mapped.toString());
+
+console.log(hello.toString());
+
+
+
