9562b9281aa4786219f25c16a6b04a8b4133a626
Ben Parisot <me@benparisot.com>
2019-04-04 16:28:19 -0700

    write smile from hex and decimal

--- buffer.js
+++ buffer.js
@@ -3,3 +3,14 @@ console.log(newBuffer);
 
 const newBufferFromString = Buffer.from('hi');
 console.log(newBufferFromString.toString());
+
+
+const buf = Buffer.alloc(9, 'Hi there!');
+console.log(buf.toString());
+
+
+const smile = Buffer.from([0xF0, 0x9F, 0x98, 0x81]);
+console.log(smile.toString());
+
+const decimalSmile = Buffer.from([240, 927, 152, 129]);
+console.log(decimalSmile.toString());
