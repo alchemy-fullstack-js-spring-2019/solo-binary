5a64d890de71f40ac7e69cea28811829481e05a6
Ben Parisot <me@benparisot.com>
2019-04-05 09:31:11 -0700

    try to get test to pass

--- tests/emojiReplacer.test.js
+++ tests/emojiReplacer.test.js
@@ -3,7 +3,6 @@ const emojiReplacer = require('../lib/emojiReplacer');
 describe('emoji replacer', () => {
   it('tests if emoji is replaced', () => {
     const buf = Buffer.from('I 😁 love 😁 emojis'); 
-    
     expect(emojiReplacer(buf, '😁', '😜')).toBe('I 😜 love 😜 emojis');
   });
 });
