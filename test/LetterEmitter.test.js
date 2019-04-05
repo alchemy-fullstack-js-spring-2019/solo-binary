89d265038006bd11d02434bc4787cff88a02fa22
Leslie Shea <leslieyshea@gmail.com>
2019-04-05 13:24:26 -0700

    LetterEmitter class with test

---
+++ test/LetterEmitter.test.js
@@ -0,0 +1,27 @@
+const LetterEmitter = require('../lib/LetterEmitter');
+
+describe('LetterEmitter class', () => {
+  let letterEmitter = null;
+
+  beforeEach(() => {
+    letterEmitter = new LetterEmitter();
+  });
+
+  it('splits a string and emits an event for each letter', done => {
+    const str = 'hiThere';
+    const letterMockHandler = jest.fn();
+    letterEmitter.on('letter', letterMockHandler);
+    letterEmitter.on('end', () => {
+      expect(letterMockHandler).toHaveBeenCalledTimes(str.length);
+      [...str].forEach((letter, offset) => {
+        expect(letterMockHandler).toHaveBeenCalledWith({
+          letter,
+          offset
+        });
+      });
+      done();
+    });
+
+    letterEmitter.read(str);
+  });
+});
