a260a70ca44db2401685e028f0775b52908f31bf
Cheri Portman <cheriportman@hotmail.com>
2019-04-05 11:13:38 -0700

    data eventemitter adds interpolation and data object

--- eeData.js
+++ eeData.js
@@ -3,7 +3,7 @@ const { EventEmitter } = require('events');
 const ee = new EventEmitter;
 
 ee.on('sit stitch', data => {
-  console.log('bark bark nope!', data);
+  console.log(`bark bark nope! ${data.response}`);
 })
 
-ee.emit('sit stitch', 'no treats!');
+ee.emit('sit stitch', { response: 'no treats'});
