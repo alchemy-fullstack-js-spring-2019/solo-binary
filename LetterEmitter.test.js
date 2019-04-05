b4c4c573bfc53ccbd6f83eb37f04e8fd01e1cddf
Cara Ponzini <ponzini@gmail.com>
2019-04-05 11:59:24 -0700

    updated tests

--- LetterEmitter.test.js
+++ LetterEmitter.test.js
@@ -19,12 +19,18 @@ describe('LetterEmitter', () => {
 
   it('splits string and emits an event for each letter', () => {
     let string = 'banana';
-    let counts = 0;
     le.read(string);
     le.on('letterFound', (data) => {
-      counts++;
-      console.log(data, counts);
-      expect(data).toEqual({ letter: 'a', offset: 1 });
+      const dataReturn = [];
+      dataReturn.push(data);
+      expect(dataReturn).toEqual([
+        { letter: 'b', offset: 0 },
+        { letter: 'a', offset: 1 },
+        { letter: 'n', offset: 2 },
+        { letter: 'a', offset: 3 },
+        { letter: 'n', offset: 4 },
+        { letter: 'a', offset: 5 }
+      ]);
     });
   });
 });
