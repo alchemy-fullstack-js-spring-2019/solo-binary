0014c67ca69832fafa8b2a1fb44fab4e2ca360b4
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-04 17:20:44 -0700

    test failing, working on function

--- tests/emojiReplacer.test.js
+++ tests/emojiReplacer.test.js
@@ -2,11 +2,13 @@ const emojiReplacer = require('../lib/emojiReplacer');
 
 describe('it replaces an emoji with another emoji', () => {
     it('replaces an emoji in a buffer with a different emoji', () => {
-        const emoji = [0xF0, 0x9F, 0x98, 0x81];
+        const emoji = '😃';
 
         const buf = Buffer.from(emoji);
         console.log(buf);
 
-        expect(emojiReplacer(buf, emoji, newEmoji)).toEqual('😍');
+        const replaceWithEmoji = '😍';
+
+        expect(emojiReplacer(buf, emoji, replaceWithEmoji)).toEqual('😍');
     });
 });
