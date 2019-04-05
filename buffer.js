ebc8cfcb13ef5083e70f25f5261b33ed7857dad6
Ben Parisot <me@benparisot.com>
2019-04-04 17:16:42 -0700

    write emoji replacer function

--- buffer.js
+++ buffer.js
@@ -12,5 +12,8 @@ console.log(buf.toString());
 const smile = Buffer.from([0xF0, 0x9F, 0x98, 0x81]);
 console.log(smile.toString());
 
-const decimalSmile = Buffer.from([240, 927, 152, 129]);
+const decimalSmile = Buffer.from([240, 159, 152, 129]);
 console.log(decimalSmile.toString());
+
+const binarySmile = Buffer.from([11110000, 10011111, 10011000, 10000001]);
+console.log(binarySmile.toString());
