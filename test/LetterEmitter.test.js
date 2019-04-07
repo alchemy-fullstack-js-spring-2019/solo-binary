87a7024044a698b196144f1ca816518063c3b5b3
dreamer <wildetangeant@gmail.com>
2019-04-07 15:29:42 -0700

    git event emit letter written

---
+++ test/LetterEmitter.test.js
@@ -0,0 +1,9 @@
+const LetterEmitter = require('./lib/letterEmitter.js');
+
+describe('LetterEmitter', () => {
+  let letterEmitter = null;
+  beforeEach(() => {
+    letterEmitter = new LetterEmitter();
+  });
+  it('splits a string and emits an event for each leter', done => )
+});
