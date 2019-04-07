316cbc559500a9bf46aac561e480def216fe96dd
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-07 16:12:40 -0700

    passing tests

--- test/LetterEmitter.test.js
+++ test/LetterEmitter.test.js
@@ -5,55 +5,26 @@ describe('LetterEmitter class', () => {
     beforeEach(() => {
         TestLetterEmitter = new LetterEmitter;
     });
+
     it('splits a string and emits an event for each letter', done => {
         const str = 'Hey there';
+        const pattern = /[a-z]/i;
         const mockEmitter = jest.fn();
         TestLetterEmitter.on('letter', mockEmitter);
-        TestLetterEmitter.on('end', () => {
-            expect(mockEmitter).toHaveBeenCalledTime(str.length);
+
+        TestLetterEmitter.once('end', () => {
+            expect(mockEmitter).toHaveBeenCalledTimes(str.length);
             [...str].forEach((letter, offset) => {
-                expect(mockEmitter).toHaveBeenCalledWith({
-                    letter,
-                    offset
-                });
+                if(pattern.test(letter)) {
+                    expect(mockEmitter).toHaveBeenCalledWith({
+                        letter,
+                        offset
+                    });
+                }
             });
+            done();
         });
-        done();
+
+        TestLetterEmitter.read(str);
     });
-
-
 });
-
-// const LetterEmitter = require('./LetterEmitter');
-
-// describe('LetterEmitter', () => {
-//   let letterEmitter = null;
-//   beforeEach(() => {
-//     letterEmitter = new LetterEmitter();
-//   });
-
-//   it('splits a string and emits an event for each letter', done => {
-//     const str = 'hi There!';
-//     const pattern = /[a-z]/i;
-//     const letterMockHandler = jest.fn();
-
-//     letterEmitter.on('letter', letterMockHandler);
-
-//     // { letter: 'h', offset: 0 }
-//     // { letter: 'i', offset: 1 }
-//     letterEmitter.once('end', () => {
-//       expect(letterMockHandler).toHaveBeenCalledTimes(7);
-//       [...str].forEach((letter, offset) => {
-//         if(pattern.test(letter)) {
-//           expect(letterMockHandler).toHaveBeenCalledWith({
-//             letter,
-//             offset
-//           });
-//         }
-//       });
-//       done();
-//     });
-
-//     letterEmitter.read(str);
-//   });
-// });
