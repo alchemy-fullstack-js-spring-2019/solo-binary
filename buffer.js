b1041547efc62a22117f00218190957d4e2a8eca
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-08 11:20:08 -0700

    fixing comma spacing

--- buffer.js
+++ buffer.js
@@ -3,18 +3,18 @@ const buf1 = Buffer.alloc(12);
 console.log('empty buffer', buf1);
 //buf.writeInt8(104, offset)
 //Hello World!
-buf1.writeInt8(72,0); // H
-buf1.writeInt8(101,1); // e
-buf1.writeInt8(108,2); // l
-buf1.writeInt8(108,3); // l
-buf1.writeInt8(111,4); // o
-buf1.writeInt8(32,5); // 
-buf1.writeInt8(87,6); // W
-buf1.writeInt8(111,7); // o
-buf1.writeInt8(114,8); // r
-buf1.writeInt8(108,9); // l
-buf1.writeInt8(100,10); // d
-buf1.writeInt8(33,11); // !
+buf1.writeInt8(72, 0); // H
+buf1.writeInt8(101, 1); // e
+buf1.writeInt8(108, 2); // l
+buf1.writeInt8(108, 3); // l
+buf1.writeInt8(111, 4); // o
+buf1.writeInt8(32, 5); // 
+buf1.writeInt8(87, 6); // W
+buf1.writeInt8(111, 7); // o
+buf1.writeInt8(114, 8); // r
+buf1.writeInt8(108, 9); // l
+buf1.writeInt8(100, 10); // d
+buf1.writeInt8(33, 11); // !
 
 console.log('Hello World!', buf1);
 console.log(buf1.toString());
