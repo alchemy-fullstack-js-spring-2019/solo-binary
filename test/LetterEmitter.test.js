af628508a471bba53d31a0fdd7e4e150e7def3c1
dreamer <wildetangeant@gmail.com>
2019-04-07 17:53:15 -0700

    tests passed, letteremitterclass works

--- test/LetterEmitter.test.js
+++ test/LetterEmitter.test.js
@@ -1,9 +1,32 @@
-const LetterEmitter = require('./lib/letterEmitter.js');
+const LetterEmitter = require('../lib/letterEmitter.js');
 
 describe('LetterEmitter', () => {
   let letterEmitter = null;
   beforeEach(() => {
     letterEmitter = new LetterEmitter();
   });
-  it('splits a string and emits an event for each leter', done => )
+  
+  it('splits a string and emits an event for each leter', done => {
+    const letterMockHandler = jest.fn();
+    const str = 'tenspacess';
+    const pattern = /[a-z]/i;
+
+    letterEmitter.on('letter', letterMockHandler);
+
+    letterEmitter.once('end', () => {
+      expect(letterMockHandler).toHaveBeenCalledTimes(10);
+      
+      str.split('').forEach((letter, offset) => {
+        if(pattern.test(letter)) {
+          expect(letterMockHandler).toHaveBeenCalledWith({
+            letter,
+            offset
+          });
+        }
+      });
+      done();
+    });
+
+    letterEmitter.read(str);
+  });
 });
