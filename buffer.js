fca118aedf2106e0fec8301c69b03a47ff04fabb
Masmics <cherlocker3113@gmail.com>
2019-04-09 20:59:31 -0700

    dev notes cleaup

--- buffer.js
+++ buffer.js
@@ -1,36 +1,24 @@
-//##  HI THERE BUFFER
 const buff = Buffer.alloc(12);
 
-//Note: buf.writeInt8(content, indexToPutIn)
-
-buff.writeInt8(72, 0);  // H
-buff.writeInt8(101, 1); // e
-buff.writeInt8(108, 2); // l
-buff.writeInt8(108, 3); // l
-buff.writeInt8(111, 4); // o
-buff.writeInt8(32, 5);  //
-buff.writeInt8(84, 6);  // T
-buff.writeInt8(104, 7); // h
-buff.writeInt8(101, 8); // e
-buff.writeInt8(114, 9); // r
-buff.writeInt8(101, 10); // e
-buff.writeInt8(33, 11);  // !
+buff.writeInt8(72, 0);
+buff.writeInt8(101, 1);
+buff.writeInt8(108, 2);
+buff.writeInt8(108, 3);
+buff.writeInt8(111, 4);
+buff.writeInt8(32, 5);
+buff.writeInt8(84, 6);
+buff.writeInt8(104, 7);
+buff.writeInt8(101, 8);
+buff.writeInt8(114, 9);
+buff.writeInt8(101, 10);
+buff.writeInt8(33, 11);
 
 console.log(buff);
 
-// output: <Buffer 48 66 6c 6c 6f 20 54 68 66 72 66 21>
-// Hello There! (in Binary).
-
 console.log(buff.toString());
 
-// output: "Hello There!"
-
-// ## BUFFER FROM A STRING
-
 const buffy = Buffer.from('Buffer The Vampire Slougher');
-console.log(buffy.toString());
-
-// # WRITE OVER BUFFER VALUES
+console.log(buffy.toString())
 
 let buffout = Buffer.from('Crawl Out Through The Fallout');
 function changeIt() {
@@ -38,12 +26,10 @@ function changeIt() {
 }
 console.log(buffout.map(changeIt));
 
-// ## BUFFER FROM HEX CODES
 
 const smile = Buffer.from([0xF0, 0x9F, 0x98, 0x81]);
 console.log(smile);
 
-// ## EMOJI REPLACER
 
 const buffish = Buffer.from('hi 😁 there 😁');
 
--- eeLetter.js
+++ eeLetter.js
@@ -9,3 +9,6 @@ const aString = 'a red car';
 [...aString].forEach(letter => {
   ee.emit('letter', letter);
 });
+
+// You need to emit data on your emitter so something like ee.emit('example', dataHere), and 
+// you need to capture that data on your listener so something like ee.on('example', captureData => {...});
