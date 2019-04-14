8966ac8ee874610eb2deaec5decfc0102ff58190
Mallarie <mallariemilano@gmail.com>
2019-04-14 15:50:41 -0700

    heart eye emoji

--- buffer.js
+++ buffer.js
@@ -1,6 +1,4 @@
-//hi there
 const buf = Buffer.alloc(10);
-console.log('buf test', buf);
 
 buf.writeInt8(0x68, 0);
 buf.writeInt8(0x69, 1);
@@ -11,12 +9,9 @@ buf.writeInt8(0x65, 5);
 buf.writeInt8(0x72, 6);
 buf.writeInt8(0x65, 7);
 
+const fromBuf = Buffer.from('Mal');
 
-//filledBuff <Buffer 4d 61 6c>
+const writeOver = buf.map(function(){});
 
-const filledBuff = Buffer.alloc(3, 'Mal');
-console.log('filledBuff', filledBuff);
-
-//const stuff = buf.writeInt8();
-
-console.log(buf.toString());
+const emoji = Buffer.from([0xF0, 0x9F, 0x98, 0x8D]);
+console.log(emoji.toString());
