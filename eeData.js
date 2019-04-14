45a4387fc4d6a766755f8d01548fa612bdec2a73
Mallarie <mallariemilano@gmail.com>
2019-04-14 16:10:33 -0700

    removed unnecessary space

--- eeData.js
+++ eeData.js
@@ -3,7 +3,7 @@ const EventEmitter = require('events');
 const ee = new EventEmitter();
 
 ee.on('we did it with data', data => {
-  console.log(`hooray ${data.name}` );
+  console.log(`hooray ${data.name}`);
 });
 
 ee.emit('we did it with data', { name: 'Mal' });
