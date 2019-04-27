8a8f8e16429923e0cdd76416382706222548a3fc
dcornelius4 <d_cornelius94@yahoo.com>
2019-04-27 14:24:33 -0700

    letterEmitter test passing

--- test/LetterEmitter.test.js
+++ test/LetterEmitter.test.js
@@ -1,5 +1,28 @@
-// const LetterEmitter = require('./LetterEmitter');
+const LetterEmitter = require('../lib/LetterEmitter');
 
-// describe('LetterEmitter', () => {
-//   let letterEmitter = null; 
-// });
+describe('LetterEmitter', () => {
+  let letterEmitter = null;
+  beforeEach(() => {
+    letterEmitter = new LetterEmitter();
+  });
+
+  it('splits a string and emits an event  for each letter', done => {
+    const str = 'hello';
+    const letterMockHandler = jest.fn();
+    letterEmitter.on('letter', letterMockHandler);
+
+    letterEmitter.on('end', () => {
+      expect(letterMockHandler).toHaveBeenCalledTimes(str.length);
+      [...str].forEach((letter, offset) => {
+        expect(letterMockHandler).toHaveBeenCalledWith({
+          letter,
+          offset
+        });
+      });
+      done();
+    });
+    letterEmitter.read(str);
+  });
+});
+
+
